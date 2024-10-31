4f27211f2eac0eaf48d93e059392d5639ad830f5
ElisaLissandra <elisa_lissandra@outlook.com>
2024-10-30 21:32:07 -0300

    Exibição das informações na página de detalhes do projeto

--- src/Screens/details_project/details_project.jsx
+++ src/Screens/details_project/details_project.jsx
@@ -1,82 +1,239 @@
-import React from "react";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+} from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { FontAwesome5, MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { db } from "../../../firebase-config";
+import { doc, getDoc } from "firebase/firestore";
 
 const DetailsProjectScreen = () => {
   const route = useRoute();
   const { project } = route.params;
-  const { title, description, generalContext, deadline, urgent, status, files = [] } = project;
+  const {
+    title,
+    description,
+    generalContext,
+    deadline,
+    urgent,
+    status,
+    files = [],
+  } = project;
+  const [user, setUser] = useState(null);
+  const navigation = useNavigation();
 
-  console.log("Datelhes projeto", project);
+  const userId = project.userId;
+  console.log(project.userId);
+
+  const getUserById = async (userId) => {
+    try {
+      const userRef = doc(db, "users", userId);
+      const userDoc = await getDoc(userRef);
+
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        console.log("Informações do usuário:", userData);
+        return userDoc.data();
+      } else {
+        throw new Error("Usuário não encontrado");
+      }
+    } catch (error) {
+      console.error("Erro ao buscar usuário:", error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userData = await getUserById(userId);
+        setUser(userData);
+      } catch (error) {
+        console.error("Erro ao buscar informações do usuário:", error);
+      }
+    };
+
+    if (userId) fetchUser();
+  }, [userId]);
+
+  const handleListProject = () => {
+    navigation.navigate("ListProject");
+  };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {/* Header */}
-      <Text style={styles.header}>{title}</Text>
-
-      {/* Section de arquivos */}
-      <Text style={styles.subHeader}>Baixa arquivo</Text>
-      {Array.isArray(files) && files.length > 0 ? (
-        files.map((file, index) => (
-          <View key={index} style={styles.fileContainer}>
-            <View style={styles.fileInfo}>
-              <Text style={styles.fileName}>{file.name}</Text>
-            </View>
-            <TouchableOpacity style={styles.downloadButton}>
-              <FontAwesome5 name="download" size={20} color="#3c3c3c" />
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "android" ? "padding" : "height"}
+      keyboardVerticalOffset={20}
+    >
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            <TouchableOpacity onPress={handleListProject}>
+              <FontAwesome5 name="chevron-left" size={24} color="#0097B2" />
             </TouchableOpacity>
+            <Text style={styles.title}>Detalhes do projeto</Text>
           </View>
-        ))
-      ) : (
-        <Text>Nenhum arquivo disponível</Text>
-      )}
 
-      {/* Data e status de urgência */}
-      <View style={styles.dateContainer}>
-        <MaterialIcons name="date-range" size={24} color="#1D9BF0" />
-        <Text style={styles.dateText}>{deadline}</Text>
-        <View style={styles.urgentContainer}>
-          <Text style={styles.urgentText}>Urgente</Text>
-          <MaterialIcons name="error" size={24} color="#FF3B30" />
+          {/* Informações do Usuário */}
+          <Text style={styles.subHeader}>Informações do Usuário</Text>
+          {/* <Text style={styles.sectionTitle}>Nome: {user?.fullName}</Text>
+          <Text style={styles.sectionTitle}>Email:  {user?.email}</Text>
+          <Text style={styles.sectionTitle}>Contato: {user?.contact}</Text>  */}
+
+          <View style={styles.userInfoContainer}>
+            <Text style={styles.sectionTitle}>Nome: </Text>
+            <Text style={styles.userInfo}>{user?.fullName}</Text>
+          </View>
+          <View style={styles.userInfoContainer}>
+            <Text style={styles.sectionTitle}>Email: </Text>
+            <Text style={styles.userInfo}>{user?.email}</Text>
+          </View>
+          <View style={styles.userInfoContainer}>
+            <Text style={styles.sectionTitle}>Contato: </Text>
+            <Text style={styles.userInfo}>{user?.contact}</Text>
+          </View>
+
+          <Text style={styles.subHeader}>Informações do Projeto</Text>
+          <Text style={styles.sectionTitle}>Título do projeto</Text>
+          <Text style={styles.titleProject}>{title}</Text>
+          {/* Section de arquivos */}
+          <Text style={styles.sectionTitle}>Baixa arquivo</Text>
+          {Array.isArray(files) && files.length > 0 ? (
+            files.map((file, index) => (
+              <View key={index} style={styles.fileContainer}>
+                <View style={styles.fileInfo}>
+                  <Text style={styles.fileName}>{file.name}</Text>
+                </View>
+                <TouchableOpacity style={styles.downloadButton}>
+                  <FontAwesome5 name="download" size={20} color="#1E1E1E" />
+                </TouchableOpacity>
+              </View>
+            ))
+          ) : (
+            <Text>Nenhum arquivo disponível</Text>
+          )}
+
+          {/* Data e status de urgência */}
+          <View style={styles.dateContainer}>
+            <FontAwesome5 name="calendar-alt" size={20} color="#0097B2" />
+            <Text style={styles.dateText}>{deadline}</Text>
+            <View style={styles.urgentContainer}>
+              <Text style={styles.urgentText}>Urgente</Text>
+              <FontAwesome5
+                name="exclamation-triangle"
+                size={20}
+                color="#E74C3C"
+                style={styles.urgentIcon}
+              />
+            </View>
+          </View>
+
+          {/* Descrição do projeto */}
+          <Text style={styles.sectionTitle}>Descrição do projeto</Text>
+          <Text style={styles.descriptionText}>{description}</Text>
+
+          {/* Contexto do projeto */}
+          <Text style={styles.sectionTitle}>Contexto do projeto</Text>
+          <Text style={styles.descriptionText}>{generalContext}</Text>
+
+          {/* Botão Acompanhar status */}
+          <TouchableOpacity style={styles.statusButton}>
+            <Text style={styles.statusButtonText}>Acompanhar status</Text>
+          </TouchableOpacity>
         </View>
-      </View>
-
-      {/* Descrição do projeto */}
-      <Text style={styles.sectionTitle}>Descrição do projeto</Text>
-      <Text style={styles.descriptionText}>
-        {description}
-      </Text>
-
-      {/* Contexto do projeto */}
-      <Text style={styles.sectionTitle}>Contexto do projeto</Text>
-      <Text style={styles.descriptionText}>
-       {generalContext}
-      </Text>
-
-      {/* Botão Acompanhar status */}
-      <TouchableOpacity style={styles.statusButton}>
-        <Text style={styles.statusButtonText}>Acompanhar status</Text>
-      </TouchableOpacity>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = {
-  container: {
+  /* container: {
     padding: 20,
     backgroundColor: "#ffffff",
   },
-  header: {
+  titleProject: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 10,
+  }, */
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    /* justifyContent: "center", */
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    padding: 20,
+    justifyContent: "flex-start",
+  },
+  headerContainer: {
+    marginTop: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 85,
+  },
+  titleProject: {
+    fontSize: 16,
+    color: "#8c8c8c",
+    marginBottom: 10,
+  },
+  /*  sectionTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  userInfo: {
+    fontSize: 16,
+    color: "#6c6c6c",
+    marginBottom: 5,
+  }, */
+  userInfoContainer: {
+    flexDirection: "row", // Alinha os filhos em linha
+    alignItems: "center", // Alinha verticalmente ao centro
+    /*  marginBottom: 5, */ // Adiciona um espaço abaixo
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: "bold", // Estilo em negrito para o título
+    marginBottom: 0, // Remover margens desnecessárias
+    color: "#333", // Cor do título
+    lineHeight: 22, // Ajuste a altura da linha para centralizar verticalmente
+  },
+  userInfo: {
+    fontSize: 16,
+    color: "#6c6c6c",
+    marginLeft: 5,
+    /* lineHeight: 22, */
+    marginTop: 15,
   },
   subHeader: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "bold",
     color: "#3c3c3c",
-    marginVertical: 10,
+    paddingVertical: 8,
+    marginTop: 10,
+    marginBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+    backgroundColor: "#f9f9f9",
+    paddingHorizontal: 5,
+    borderRadius: 5,
   },
   fileContainer: {
     flexDirection: "row",
@@ -96,7 +253,7 @@ const styles = {
   },
   fileDetails: {
     fontSize: 14,
-    color: "#8c8c8c",
+    color: "#6c6c6c",
   },
   downloadButton: {
     padding: 5,
@@ -130,12 +287,12 @@ const styles = {
     marginBottom: 5,
   },
   descriptionText: {
-    fontSize: 14,
+    fontSize: 16,
     color: "#6c6c6c",
     marginBottom: 15,
   },
   statusButton: {
-    backgroundColor: "#1D9BF0",
+    backgroundColor: "#0097B2",
     padding: 15,
     alignItems: "center",
     borderRadius: 8,
